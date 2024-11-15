a5154743d624ef8898a85faffd670caf578cffc9
vlmsgnkag <voleminhdz@gmail.com>
2024-11-15 20:33:17 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector(".card-btn");
-const card = document.querySelector(".card");
-
-button.addEventListener("click", () => {
-  card.classList.toggle("change");
-});
